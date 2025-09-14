0fed083d39d919ed5a80f507cc2efce2220a7e2c
Tom ten Thij <tomtenthij+git@gmail.com>
2025-09-14 15:24:50 +0200

    (disabled) LSP navigation: keymaps + tagfunc for Ctrl-]
    Suggested but does folding when not desired, abandon for now

--- lua/config.lua
+++ lua/config.lua
@@ -12,5 +12,20 @@ require('nvim-treesitter.configs').setup{
   indent    = { enable = true },
 }
 -- Treesitter folds
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr   = 'nvim_treesitter#foldexpr()'
+-- vim.o.foldmethod = 'expr'
+-- vim.o.foldexpr   = 'nvim_treesitter#foldexpr()'
+--
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(ev)
+--     local buf = ev.buf
+--     local map = function(mode, lhs, rhs) vim.keymap.set(mode, lhs, rhs, {buffer = buf}) end
+--     map('n','gd', vim.lsp.buf.definition)
+--     map('n','gD', vim.lsp.buf.declaration)
+--     map('n','gr', vim.lsp.buf.references)
+--     map('n','gi', vim.lsp.buf.implementation)
+--     map('n','K',  vim.lsp.buf.hover)
+--     map('n','<leader>rn', vim.lsp.buf.rename)
+--     -- Also make Ctrl-] use LSP tags:
+--     vim.bo[buf].tagfunc = 'v:lua.vim.lsp.tagfunc'
+--   end
+-- })
